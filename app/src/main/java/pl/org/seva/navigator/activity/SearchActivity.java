e942f9013d8ff5ac2bbc23b3d11647a801d65236
wiktor <wiktor.nizio@gmail.com>
2017-03-01 16:05:07 +0100

    Added search mock ups.

--- app/src/main/java/pl/org/seva/navigator/activity/SearchActivity.java
+++ app/src/main/java/pl/org/seva/navigator/activity/SearchActivity.java
@@ -17,7 +17,62 @@
 
 package pl.org.seva.navigator.activity;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import pl.org.seva.navigator.R;
 
 public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            search(query);
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            search(query);
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_overflow_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_search) {
+            onSearchRequested();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void search(String query) {
+
+    }
 }
