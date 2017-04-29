c5e0acbc71020f03009d8a65c61c8700288f8664
wiktor <wiktor.nizio@gmail.com>
2017-04-29 22:57:48 +0200

    Corrected location update frequency.

--- app/src/main/java/pl/org/seva/navigator/presenter/source/MyLocationSource.java
+++ app/src/main/java/pl/org/seva/navigator/presenter/source/MyLocationSource.java
@@ -23,7 +23,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
-import android.util.Pair;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
