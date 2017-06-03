5d2e49040cd167489705d3adda8cc2402ac22ba9
wiktor <wiktor.nizio@gmail.com>
2017-06-03 18:02:56 +0200

    Converted tests to Kotlin.

--- navigator/src/androidTest/java/pl/org/seva/navigator/ExampleInstrumentedTest.kt
+++ navigator/src/androidTest/java/pl/org/seva/navigator/ExampleInstrumentedTest.kt
@@ -15,29 +15,29 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package pl.org.seva.navigator;
+package pl.org.seva.navigator
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.Test
+import org.junit.runner.RunWith
 
-import static org.junit.Assert.*;
+import org.junit.Assert.*
 
 /**
  * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("pl.org.seva.navigator", appContext.getPackageName());
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    @Throws(Exception::class)
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+        assertEquals("pl.org.seva.navigator", appContext.packageName)
     }
 }
---
+++ navigator/src/test/java/pl/org/seva/navigator/FirebaseTest.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2017 Wiktor Nizio
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pl.org.seva.navigator
+
+import com.google.android.gms.maps.model.LatLng
+
+import org.junit.Test
+
+import pl.org.seva.navigator.model.database.firebase.FirebaseBase
+
+import org.junit.Assert.assertEquals
+
+class FirebaseTest {
+
+    @Test
+    fun latLng2String() {
+        val lat = java.lang.Double.parseDouble(LAT)
+        val lon = java.lang.Double.parseDouble(LON)
+        val str = FirebaseBase.latLng2String(LatLng(lat, lon))
+        assertEquals(LAT + ";" + LON, str)
+    }
+
+    @Test
+    fun string2LatLng() {
+        val latLng = FirebaseBase.string2LatLng(LAT + ";" + LON)
+        assertEquals(LAT, java.lang.Double.toString(latLng.latitude))
+        assertEquals(LON, java.lang.Double.toString(latLng.longitude))
+    }
+
+    companion object {
+
+        private val LAT = "54.5922815"
+        private val LON = "-5.9634933"
+    }
+}
