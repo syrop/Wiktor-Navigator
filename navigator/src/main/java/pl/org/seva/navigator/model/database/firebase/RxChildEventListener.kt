9de1662404b5e59ccebe9b7d4e9b8a2aa9c7067c
wiktor <wiktor.nizio@gmail.com>
2017-06-24 20:35:38 +0200

    Allowed second parameter of a method to be null in an implementation of ChildEventListener.

--- navigator/src/main/java/pl/org/seva/navigator/model/database/firebase/RxChildEventListener.kt
+++ navigator/src/main/java/pl/org/seva/navigator/model/database/firebase/RxChildEventListener.kt
@@ -26,15 +26,15 @@ import io.reactivex.subjects.ReplaySubject
 internal class RxChildEventListener(private val childEventSubject: ReplaySubject<DataSnapshot>) :
         ChildEventListener {
 
-    override fun onChildAdded(dataSnapshot: DataSnapshot, s: String) {
+    override fun onChildAdded(dataSnapshot: DataSnapshot, s: String?) {
         childEventSubject.onNext(dataSnapshot)
     }
 
-    override fun onChildChanged(dataSnapshot: DataSnapshot, s: String) {}
+    override fun onChildChanged(dataSnapshot: DataSnapshot, s: String?) {}
 
     override fun onChildRemoved(dataSnapshot: DataSnapshot) {}
 
-    override fun onChildMoved(dataSnapshot: DataSnapshot, s: String) {}
+    override fun onChildMoved(dataSnapshot: DataSnapshot, s: String?) {}
 
     override fun onCancelled(databaseError: DatabaseError) {
     }
