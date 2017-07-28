c0e22cce66591c53e22a5242446f612008a782d9
wiktor <wiktor.nizio@gmail.com>
2017-07-28 11:02:40 +0200

    Suppressed deprecation.

--- navigator/src/main/java/pl/org/seva/navigator/view/builder/notification/PeerRequestedFriendshipNotificationBuilder.kt
+++ navigator/src/main/java/pl/org/seva/navigator/view/builder/notification/PeerRequestedFriendshipNotificationBuilder.kt
@@ -15,6 +15,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+@file:Suppress("DEPRECATION")
+
 package pl.org.seva.navigator.view.builder.notification
 
 import android.app.Notification
