9022143d2e4fc4f88ad2b9fd1002098bbab26434
wiktor <wiktor.nizio@gmail.com>
2018-01-22 00:47:47 +0100

    Increased minimal confidence required to detect that the device is stationary.

--- navigator/src/main/kotlin/pl/org/seva/navigator/main/ActivityRecognitionSource.kt
+++ navigator/src/main/kotlin/pl/org/seva/navigator/main/ActivityRecognitionSource.kt
@@ -133,7 +133,7 @@ open class ActivityRecognitionSource :
         private const val ACTIVITY_RECOGNITION_INTENT = "activity_recognition_intent"
         private const val ACTIVITY_RECOGNITION_INTERVAL_MS = 1000L
         /** The device is only stationary if confidence >= this level. */
-        private const val MIN_CONFIDENCE = 75 // 70 does not work on LG G6 Android 7.0
+        private const val MIN_CONFIDENCE = 80 // 70 does not work on LG G6 Android 7.0
 
         private val stationarySubject = PublishSubject.create<Any>()
         private val movingSubject = PublishSubject.create<Any>()
