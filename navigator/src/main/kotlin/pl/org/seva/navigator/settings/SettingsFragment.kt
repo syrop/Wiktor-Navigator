1bcc968117fafa15dfc4a8d3eee756df93e0bd8f
wiktor <wiktor.nizio@gmail.com>
2018-04-18 22:14:45 +0200

    Added a listener on clicking preferences in SettingsFragment.

--- navigator/src/main/kotlin/pl/org/seva/navigator/settings/SettingsFragment.kt
+++ navigator/src/main/kotlin/pl/org/seva/navigator/settings/SettingsFragment.kt
@@ -1,6 +1,7 @@
 package pl.org.seva.navigator.settings
 
 import android.os.Bundle
+import android.support.v7.preference.Preference
 import android.support.v7.preference.PreferenceFragmentCompat
 import pl.org.seva.navigator.R
 
@@ -8,5 +9,16 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.preferences)
+    }
+
+    override fun onPreferenceTreeClick(preference: Preference): Boolean {
+        return when (preference.key) {
+            DEBUG -> true
+            else -> super.onPreferenceTreeClick(preference)
+        }
+    }
+
+    companion object {
+        const val DEBUG = "debug"
     }
 }
