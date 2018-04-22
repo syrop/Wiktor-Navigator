68da33108d06074258cb3cef21cfc5d5c72aa85c
wiktor <wiktor.nizio@gmail.com>
2018-04-22 23:05:44 +0200

    Added displaying a dialog when debug mode is turned on.

--- navigator/src/main/kotlin/pl/org/seva/navigator/settings/SettingsFragment.kt
+++ navigator/src/main/kotlin/pl/org/seva/navigator/settings/SettingsFragment.kt
@@ -1,6 +1,8 @@
 package pl.org.seva.navigator.settings
 
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.preference.CheckBoxPreference
 import android.support.v7.preference.Preference
 import android.support.v7.preference.PreferenceFragmentCompat
 import pl.org.seva.navigator.R
@@ -11,12 +13,24 @@ class SettingsFragment : PreferenceFragmentCompat() {
         addPreferencesFromResource(R.xml.preferences)
     }
 
-    override fun onPreferenceTreeClick(preference: Preference): Boolean {
-        return when (preference.key) {
-            DEBUG -> true
+    private fun onDebugClicked(checkbox: CheckBoxPreference) {
+        if (!checkbox.isChecked) {
+            return
+        }
+        val builder = AlertDialog.Builder(activity!!, android.R.style.Theme_Material_Dialog_Alert)
+        builder.setMessage(R.string.settings_activity_debug_info)
+                .setTitle(R.string.pref_debug_mode)
+                .setPositiveButton(android.R.string.ok) { dialog, _ -> dialog.dismiss() }
+                .show()
+    }
+
+    override fun onPreferenceTreeClick(preference: Preference) = when (preference.key) {
+            DEBUG -> {
+                onDebugClicked(preference as CheckBoxPreference)
+                true
+            }
             else -> super.onPreferenceTreeClick(preference)
         }
-    }
 
     companion object {
         const val DEBUG = "debug"
