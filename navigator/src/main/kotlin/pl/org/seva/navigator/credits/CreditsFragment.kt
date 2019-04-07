5782eb3daca7cf5cf1a686589fd847bf2b5a2cb1
wiktor <wiktor.nizio@gmail.com>
2019-04-07 14:42:49 +0200

    Removed an unused import.

--- navigator/src/main/kotlin/pl/org/seva/navigator/credits/CreditsFragment.kt
+++ navigator/src/main/kotlin/pl/org/seva/navigator/credits/CreditsFragment.kt
@@ -26,7 +26,6 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_credits.*
